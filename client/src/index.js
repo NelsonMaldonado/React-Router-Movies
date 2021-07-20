a1474bffb4ea21998b4fbf74640a5586849af046
Nelson Maldonado <play2nino@gmail.com>
2021-07-19 23:58:52 -0700

    saving again

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-
+const theme = 'white';
 // You'll need to wrap <App /> for routing to work
 ReactDOM.render(<App />, document.getElementById('root'));
